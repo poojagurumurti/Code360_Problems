6b0dd1c20d5cce4651f0f7970b6adbec7c89087a
Pooja Tamragouri <98204285+poojagurumurti@users.noreply.github.com>
2025-03-11 16:25:40 +0530

    Create  Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,45 @@
+# Problem statement
+# Send feedback
+# You are given an integer array 'A' of size 'N', sorted in non-decreasing order. You are also given an integer 'target'. Your task is to write a function to search for 'target' in the array 'A'. If it exists, return its index in 0-based indexing. If 'target' is not present in the array 'A', return -1.
+
+
+
+# Note:
+# You must write an algorithm whose time complexity is O(LogN)
+
+
+# Detailed explanation ( Input/output format, Notes, Images )
+# Constraints :
+# 1 <= N <= 10^5
+# 1 <= A[i] <= 10^9
+# 1 <= target <= 10^9
+# Time Limit: 1 sec
+# Sample Input 1:
+# 7
+# 1 3 7 9 11 12 45
+# 3
+# Sample Output 1:
+# 1
+# Explanation of sample output 1:
+# nums = [1, 3, 7, 9, 11, 12, 45],
+# The index of element '3' is 1.
+# Hence, the answer is '1'.
+
+
+# Sample Input 2:
+# 7
+# 1 2 3 4 5 6 7
+# 9
+# Sample Output 2:
+# -1
+# Explanation of sample output 2:
+# nums = [1, 2, 3, 4, 5, 6, 7],
+# Element '9' doesn't exist.
+# Hence, the answer is '-1'.
+
+def search(nums: [int], target: int):
+    for i in range(len(nums)):
+        if nums[i] ==target:
+            return i
+    return -1
+
